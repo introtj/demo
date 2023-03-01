d6a3f51e41db19b57c5a130ffc8ed5545b57cccb
gdseo <gdseo@smilegate.com>
2023-03-01 14:35:29 +0900

    .

--- src/main/java/com/example/demo1/exception/ErrorMessage.java
+++ src/main/java/com/example/demo1/exception/ErrorMessage.java
@@ -1,33 +1,18 @@
 package com.example.demo1.exception;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class ErrorMessage {
 
-    private int code;
-    private String message;
+    private int errorCode;
+    private String errorMessage;
 
-    public ErrorMessage() {
 
-    }
-
-    public ErrorMessage(int code, String message) {
-        super();
-        this.code = code;
-        this.message = message;
-    }
-
-    public int getCode() {
-        return code;
-    }
-
-    public void setCode(int code) {
-        this.code = code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
+    public ErrorMessage(int errorCode, String errorMessage) {
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
     }
 }
