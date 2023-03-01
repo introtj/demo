02ae06feacb8dbdecd467535a28ee17e9a1a5c0a
gdseo <gdseo@smilegate.com>
2023-03-01 14:05:40 +0900

    .

--- src/main/java/com/example/demo1/controller/MovieController.java
+++ src/main/java/com/example/demo1/controller/MovieController.java
@@ -1,5 +1,12 @@
 package com.example.demo1.controller;
 
+import java.util.Optional;
+
+import com.example.demo1.domain.Movie;
+import com.example.demo1.service.MovieService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,6 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MovieController {
 
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
 
     @GetMapping(path = "/hello")
     public String hello(@RequestParam(name = "message") String message) {
@@ -15,8 +27,13 @@ public class MovieController {
     }
 
 
-    @GetMapping(path = "/movie/{id}")
-    public String getMovie(@PathVariable(name = "id") String id) {
-        return "";
+    @GetMapping(path = "/movies/{id}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public Movie getMovie(@PathVariable(name = "id") String id) {
+        return this.movieService.getMovieById(id).get();
+    }
+
+    @GetMapping(path = "/movies", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public Page<Movie> getMovies(Pageable pageable) {
+        return this.movieService.getMovies(pageable);
     }
 }
